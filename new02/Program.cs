530c34e767a7a65ab8a151988ec0bea79010a481
Zemburych <116352585+Zemburych@users.noreply.github.com>
2022-10-31 12:26:11 +0300

    добавил блок схему

---
+++ new02/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+
+int[] array = new int[3];
+// Рандомный ввод массива
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+   array[i] = rand.Next(0,9);
+//Вывод массива 
+            Console.WriteLine();
+            Console.Write("Исходное число ");
+            for (int i = 0; i < array.Length; i++)
+            Console.Write(array[i]);
+            Console.WriteLine();
+if (array.Length > 2)
+{
+  Console.WriteLine("третья цифра -> " + array[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
